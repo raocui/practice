a1b0c3884a9fd10390846b76100afc5e266825d1
raocui <raocui@rp-pet.cn>
2023-11-13 08:47:33 +0800

    提交

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
-module github.com/raocui/practice
+module _
 
-go 1.17
+go 1.21.1
+
+require (
+	github.com/streadway/amqp v1.1.0
+	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6
+	gopl.io v0.0.0-20211004154805-1ae3ec64947b
+)
--- main.go
+++ main.go
@@ -4,8 +4,52 @@ import (
 	"fmt"
 )
 
+// 括号匹配的检验： 检验括号是否匹配的方法用“期待的急迫程度”这个概念来描述
+// 分析可能出现的不匹配的情况:
+// 到来的右括弧非是所“期待”的
+// 到来的是“不速之客”
+// 直到结束，也没有到来所“期待的
+// 正确的格式： ([]()) 或 [([][])]
 func main() {
 
-	fmt.Print("first line")
-	fmt.Print("second line")
+}
+
+func matching(str string) error {
+	sli := []rune(str)
+	data := make([]string, 0)
+flag:
+	for _, v := range sli {
+		c := string(v)
+		switch c {
+		case "(", "[", "{":
+			data = append(data, c)
+		case ")", "]", "}":
+			if len(data) < 1 {
+				return fmt.Errorf("不速之客， 右边的括号多于左边")
+			}
+			pop := data[len(data)-1]
+			data = data[:len(data)-1]
+			switch c {
+			case ")":
+				if pop != "(" {
+					goto flag
+				}
+			case "]":
+				if pop != "[" {
+					goto flag
+				}
+			case "}":
+				if pop != "{}" {
+					goto flag
+				}
+			}
+
+		default:
+			return fmt.Errorf("非法括号：%d", c)
+
+		}
+	}
+
+	if len(data)>0{}
+
 }
